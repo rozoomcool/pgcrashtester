1d39e6a143f729be8e1d2a778a21dc262088e763
rozoomcool <rozoom.um@gmail.com>
2023-11-04 08:33:37 +0300

    .

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ def bench():
         return jsonify({'error': 'ты че? махаться будешь?'})
     data = request.json or {}
     dbname = data.get('dbname', 'lamtech_db')
-    scale_factor = data.get('scaleFactor', '10')
-    clients = data.get('clients', '100')
+    scale_factor = data.get('scaleFactor', '1')
+    clients = data.get('clients', '1')
     threads = data.get('threads', '2')
-    transactions = data.get('transactions', '50000')
+    transactions = data.get('transactions', '1')
     # Установите переменные окружения для pgbench
     os.environ['PGUSER'] = 'postgres'
     os.environ['PGPASSWORD'] = 'root'
     # Инициализация базы данных для тестирования
-    init_cmd = ['sudo', '-u', 'postgres', 'pgbench', '-i', '-s', scale_factor, dbname]
-    subprocess.run(init_cmd, check=True)
+    # init_cmd = ['sudo', '-u', 'postgres', 'pgbench', '-i', '-s', scale_factor, dbname]
+    # subprocess.run(init_cmd, check=True)
     # Запуск теста
     run_cmd = ['sudo', '-u', 'postgres', 'pgbench', '-c', clients, '-j', threads, '-t', transactions, dbname]
     process = subprocess.run(run_cmd, capture_output=True, text=True, check=True)
